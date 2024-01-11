b5de87d3f09a9f01461d5df44aec5ac77a1982e5
DimaMinin <91825998+DimaMinin@users.noreply.github.com>
2024-01-11 20:01:13 +0300

    try something

--- src/test/java/com/minin/diarybackend/DiaryBackendApplicationTests.java
+++ src/test/java/com/minin/diarybackend/DiaryBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.minin.diarybackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DiaryBackendApplicationTests.class)
 class DiaryBackendApplicationTests {
 
 	@Test
